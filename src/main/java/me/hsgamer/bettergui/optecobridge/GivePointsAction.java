fef3bdf6c0a0a534c1a29cce0c8685d9f996c2f8
HSGamer <huynhqtienvtag@gmail.com>
2022-08-17 17:41:09 +0700

    update to BetterGUI 7.0

--- src/main/java/me/hsgamer/bettergui/optecobridge/GivePointsAction.java
+++ src/main/java/me/hsgamer/bettergui/optecobridge/GivePointsAction.java
@@ -1,43 +1,41 @@
 package me.hsgamer.bettergui.optecobridge;
 
+import me.hsgamer.bettergui.BetterGUI;
 import me.hsgamer.bettergui.api.action.BaseAction;
-import me.hsgamer.bettergui.config.MessageConfig;
-import me.hsgamer.bettergui.lib.core.bukkit.utils.MessageUtils;
-import me.hsgamer.bettergui.lib.core.common.Validate;
-import me.hsgamer.bettergui.lib.core.expression.ExpressionUtils;
-import me.hsgamer.bettergui.lib.taskchain.TaskChain;
+import me.hsgamer.bettergui.builder.ActionBuilder;
+import me.hsgamer.hscore.common.Validate;
+import me.hsgamer.hscore.task.BatchRunnable;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
-import java.util.Objects;
+import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 
 public final class GivePointsAction extends BaseAction {
-
-    public GivePointsAction(String string) {
-        super(string);
+    public GivePointsAction(ActionBuilder.Input input) {
+        super(input);
     }
 
     @Override
-    public void addToTaskChain(UUID uuid, TaskChain<?> taskChain) {
-        int pointsToGive = 0;
-        String replaced = getReplacedString(uuid);
-        if (Validate.isValidPositiveNumber(replaced)) {
-            pointsToGive = Integer.parseInt(replaced);
-        } else if (ExpressionUtils.isValidExpression(replaced)) {
-            pointsToGive = Objects.requireNonNull(ExpressionUtils.getResult(replaced)).intValue();
-        } else {
-            Optional.ofNullable(Bukkit.getPlayer(uuid)).ifPresent(player -> MessageUtils.sendMessage(player, MessageConfig.INVALID_NUMBER.getValue().replace("{input}", replaced)));
+    public void accept(UUID uuid, BatchRunnable.Process process) {
+        String parsed = getReplacedString(uuid);
+        Optional<Double> optionalPoint = Validate.getNumber(parsed).map(BigDecimal::doubleValue);
+        if (!optionalPoint.isPresent()) {
+            Optional.ofNullable(Bukkit.getPlayer(uuid)).ifPresent(player -> player.sendMessage(ChatColor.RED + "Invalid point amount: " + parsed));
+            process.next();
+            return;
         }
-
-        if (pointsToGive > 0) {
-            int finalPointsToGive = pointsToGive;
-            taskChain.sync(() -> {
-                if (!OptEcoBridge.givePoints(uuid, finalPointsToGive)) {
+        double pointToGive = optionalPoint.get();
+        if (pointToGive > 0) {
+            Bukkit.getScheduler().runTask(BetterGUI.getInstance(), () -> {
+                if (!OptEcoBridge.givePoints(uuid, pointToGive)) {
                     Optional.ofNullable(Bukkit.getPlayer(uuid)).ifPresent(player -> player.sendMessage(ChatColor.RED + "Error: the transaction couldn't be executed. Please inform the staff."));
                 }
+                process.next();
             });
+        } else {
+            process.next();
         }
     }
 }
--- src/main/java/me/hsgamer/bettergui/optecobridge/PointRequirement.java
+++ src/main/java/me/hsgamer/bettergui/optecobridge/PointRequirement.java
@@ -1,31 +1,27 @@
 package me.hsgamer.bettergui.optecobridge;
 
+import me.hsgamer.bettergui.BetterGUI;
 import me.hsgamer.bettergui.api.requirement.TakableRequirement;
-import me.hsgamer.bettergui.config.MessageConfig;
-import me.hsgamer.bettergui.lib.core.bukkit.utils.MessageUtils;
-import me.hsgamer.bettergui.lib.core.expression.ExpressionUtils;
-import me.hsgamer.bettergui.lib.core.variable.VariableManager;
-import me.hsgamer.bettergui.manager.PluginVariableManager;
+import me.hsgamer.bettergui.builder.RequirementBuilder;
+import me.hsgamer.bettergui.util.StringReplacerApplier;
+import me.hsgamer.hscore.bukkit.utils.MessageUtils;
+import me.hsgamer.hscore.common.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
 public final class PointRequirement extends TakableRequirement<Double> {
-    private final Map<UUID, Double> checked = new HashMap<>();
-
-    public PointRequirement(String name) {
-        super(name);
-        PluginVariableManager.register(name, (original, uuid) -> {
-            double points = getParsedValue(uuid);
+    public PointRequirement(RequirementBuilder.Input input) {
+        super(input);
+        getMenu().getVariableManager().register(getName(), (original, uuid) -> {
+            double points = getFinalValue(uuid);
             if (points > 0 && !OptEcoBridge.hasPoints(uuid, points)) {
                 return String.valueOf(points);
             }
-            return MessageConfig.HAVE_MET_REQUIREMENT_PLACEHOLDER.getValue();
+            return BetterGUI.getInstance().getMessageConfig().haveMetRequirementPlaceholder;
         });
     }
 
@@ -40,29 +36,24 @@ public final class PointRequirement extends TakableRequirement<Double> {
     }
 
     @Override
-    protected void takeChecked(UUID uuid) {
-        if (!OptEcoBridge.takePoints(uuid, checked.remove(uuid))) {
-            Optional.ofNullable(Bukkit.getPlayer(uuid)).ifPresent(player -> player.sendMessage(ChatColor.RED + "Error: the transaction couldn't be executed. Please inform the staff."));
-        }
-    }
-
-    @Override
-    public Double getParsedValue(UUID uuid) {
-        String parsed = VariableManager.setVariables(String.valueOf(value).trim(), uuid);
-        return Optional.ofNullable(ExpressionUtils.getResult(parsed)).map(BigDecimal::doubleValue).orElseGet(() -> {
-            Optional.ofNullable(Bukkit.getPlayer(uuid)).ifPresent(player -> MessageUtils.sendMessage(player, MessageConfig.INVALID_NUMBER.getValue().replace("{input}", parsed)));
+    protected Double convert(Object value, UUID uuid) {
+        String parsed = StringReplacerApplier.replace(String.valueOf(value).trim(), uuid, this);
+        return Validate.getNumber(parsed).map(BigDecimal::doubleValue).orElseGet(() -> {
+            Optional.ofNullable(Bukkit.getPlayer(uuid)).ifPresent(player -> MessageUtils.sendMessage(player, BetterGUI.getInstance().getMessageConfig().invalidNumber.replace("{input}", parsed)));
             return 0D;
         });
     }
 
     @Override
-    public boolean check(UUID uuid) {
-        double points = getParsedValue(uuid);
-        if (points > 0 && !OptEcoBridge.hasPoints(uuid, points)) {
-            return false;
-        } else {
-            checked.put(uuid, points);
-            return true;
+    protected Result checkConverted(UUID uuid, Double value) {
+        if (value > 0 && !OptEcoBridge.hasPoints(uuid, value)) {
+            return Result.fail();
         }
+        return successConditional((uuid1, process) -> Bukkit.getScheduler().runTask(BetterGUI.getInstance(), () -> {
+            if (!OptEcoBridge.takePoints(uuid1, value)) {
+                Optional.ofNullable(Bukkit.getPlayer(uuid1)).ifPresent(player -> player.sendMessage(ChatColor.RED + "Error: the transaction couldn't be executed. Please inform the staff."));
+            }
+            process.next();
+        }));
     }
 }
